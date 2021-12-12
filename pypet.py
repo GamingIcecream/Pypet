dee89e5dc662deadc8e165b4d5c4eaa0ded8ac8e
Icecream629 <rohblockmaster@gmail.com>
2021-12-12 12:18:46 -0800

    lots of edits

--- pypet.py
+++ pypet.py
@@ -2,8 +2,10 @@ import random
 from time import sleep
 py_cat = {
     'photo': '(=^o.o^=)__',
+    'full': '(=^x.x^=)__',
+    'sleep': '(=^+.+^=)__',
     'name': 'Mochi',
-    'age': 5,
+    'age': 1,
     'weight': 5.7,
     'hungry': True,
     'phrases': ["Purrr", "*lick* *lick*", "Meow! Mew!"]
@@ -34,6 +36,7 @@ def pypet_stats():
     print (py_cat['photo'])    
 
     print (py_cat['name'] + " weighs " + str(py_cat['weight']) + " pounds ")
+    print (py_cat['name'] + " is " + str(py_cat['age']) + " years old")
 
     if py_cat['hungry']:
         print ("Your pypet is hungry!")
@@ -44,7 +47,7 @@ startup_pypet()
 # pypet_stats()
 
 terminate = False
-
+sleepNum = 0
 while not terminate:
     print ("#####################################")
 
@@ -60,24 +63,56 @@ while not terminate:
           py_cat['weight'] = py_cat['weight'] + 1.5
           py_cat['hungry'] = False
         else:
-            print("(=^x.x^=)__")
+            print(py_cat['full'])
             print("TOO full... *BURP* ...")
     elif user_input == "chat":
         print (random.choice(py_cat['phrases']))
     elif user_input == "name":
         print ("What do you want to name your cat?")
-        print ("(=^o.o^=)__")
+        print (py_cat['photo'])
         py_cat['name'] = input()
         print(py_cat['name'] + " learned its new name!")
     elif user_input == "help":
-        print("You can use the [stats, feed, chat, name, quit, help, sleep] commands to do things with your pypet!")
+        print("You can use the [stats, feed, chat, name, quit, help, sleep, play] commands to do things with your pypet!")
+    elif user_input == "play":
+        print(py_cat['photo'])
+        print("Its time to play! Want to play [guess the number, other]?")
+        game = input('> ')
+        if game == "guess the number":
+            tries = 1
+            print("Lets play guess the number!")
+            number = ['1','2','3','4','5','6','7','8','9','10']
+            print("I am thinking of a number 1-10. Guess what it is! You have 3 tries.")
+            trueNum = random.choice(number)
+            guessNum = input()
+            if guessNum == trueNum:
+                print("You got it! Lets play again later.")
+                py_cat['hungry'] = True
+            while not guessNum == trueNum:
+                tries = tries + 1
+                print("Nope, thats not it, try again.")
+                guessNum = input()
+                if guessNum == trueNum:
+                    print("You got it! Lets play again later.")
+                    py_cat['hungry'] = True
+                    py_cat['weight'] = py_cat['weight'] - 0.4
+                if tries == 3:
+                    print("Your tries are up! The correct answer was: " + trueNum)
+                    guessNum = trueNum
+        elif game == "other":
+            print("Coming soon. Lets play later")
     elif user_input == "sleep":
-        print ("(=^+.+^=)__")
+        print (py_cat['sleep'])
         print ("Zzzzz...")
-        sleep(30)
-        print ("(=^o.o^=)__")
+        sleep(1)
+        print (py_cat['photo'])
         print ("Yawn!!")
         py_cat['hungry'] = True
+        sleepNum = sleepNum + 1
+        if sleepNum == 10:
+            py_cat['age'] = py_cat['age'] + 1
+            sleepNum = 0
+            print(py_cat['name'] + " grew up!")
     else:
         print ("Sorry, I don't understand")
     
